dafcd97f162c16732094a15abaf0730419c47a48
unknown <minaraouf1997@gmail.com>
2022-02-10 16:45:32 +0200

    adding property orm

--- products/models.py
+++ products/models.py
@@ -1,9 +1,37 @@
+#from typing_extensions import blank
 from django.db import models
 
 from users.models import User
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=70, blank=False, default='')
-    price = models.IntegerField(blank=False, default='')
+
+
+class Governorate(models.Model):
+    name = models.CharField(max_length=30)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Property(models.Model):
+  #  name = models.CharField(max_length=70, blank=False, default='')
+
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    governorate = models.ForeignKey(Governorate,on_delete=models.CASCADE ,blank=False)
+    area = models.CharField(max_length=30,blank=False)
+    street = models.CharField(max_length=30, blank=False)
+    building_number = models.IntegerField(blank=False)
+    propert_number = models.IntegerField(blank=False)
+    
+    price = models.IntegerField( default='',blank=False)
+    describiton = models.TextField()
+    size = models.CharField(max_length=20,blank=False)
+
+    
+
+
+
+
+    
--- products/serializers.py
+++ products/serializers.py
@@ -1,20 +1,24 @@
-from rest_framework import serializers 
-from products.models import Product
+from rest_framework import serializers
+from products.models import Governorate 
+from products.models import Property
 from users.serializers import UserSerializer
 from users.models import User
 
-class ProductSerializer(serializers.ModelSerializer):
-    seller_name = serializers.SerializerMethodField()
-
-    def get_seller_name(self, obj):
+class PropertySerializer(serializers.ModelSerializer):
+   # seller = serializers.SerializerMethodField()
+  #  governorate_id = serializers.RelatedField(source='governorate',read_only=True)
+   # seller_id = serializers.RelatedField(source='seller',read_only=True)
+    seller = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(),many=False)
+    governorate = serializers.PrimaryKeyRelatedField(queryset=Governorate.objects.all(),many=False)
+ 
+    def get_seller(self, obj):
         return obj.seller.name
-    class Meta:
-        model = Product
-        fields = ('id','name','price','seller','seller_name')
     
-    # def create(self,validate_data):
-    #     validate_data['seller_id']=1
-    #     print(validate_data)
-    #     product = Product.objects.create(**validate_data)
-    #     # print(product)
-    #     return product
+    def get_governorate(self, obj):
+        return obj.governorate.name
+    
+    class Meta:
+        model = Property
+        fields = ('id','describiton','price','seller','size','governorate' , 'area','street','building_number','propert_number')
+
+	
